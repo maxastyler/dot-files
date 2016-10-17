f85bdb1a20438428f39060d0873710cb1c99ea87
Max Tyler <mtyler88@gmail.com>
2016-10-17 15:28:15 +0100

    setup.py loads the json file locations.json

--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
+#!/bin/python3
 import argparse
+import json
+import os
+
+class prog_files:
+    def __init__(self, program, files, scripts):
+        self.program=program
+        self.files=files
+        self.scripts=scripts
+
+    def print_files(self):
+        for thing in self.files:
+            print(thing)
+
+    def write_files(self, backup=True):
+        
+
+backup_folder="~/.mtyler_dot_backup"
 
 def install():
-    pass
+    with open('locations.json') as data_file:
+        data=json.load(data_file)
+    prog_categories=[]
+    for prog_type in data:
+        prog_categories.append(prog_files(prog_type, data[prog_type]["files_to_link"], data[prog_type]["scripts_to_run"]))
+    for i in prog_categories:
+        i.print_files()
 
 def uninstall():
     pass
