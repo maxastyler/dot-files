90bb147fdd4c171c4a25d73db82767707c6f5cad
Max Tyler <mtyler88@gmail.com>
2016-10-17 16:43:43 +0100

    The setup.py can now install files, and back up previous configs. Also warns of overwriting backups

--- setup.py
+++ setup.py
@@ -2,21 +2,45 @@
 import argparse
 import json
 import os
+import shutil
+import sys
+
+backup_folder=os.path.expanduser("~/.mtyler_dotfiles_backup")
 
 class prog_files:
     def __init__(self, program, files, scripts):
         self.program=program
+        self.files={}
+        self.scripts={}
         self.files=files
+        for thing in self.files:
+            self.files[thing]=os.path.expanduser(self.files[thing])
+        for thing in self.scripts:
+            self.scripts[thing]=os.path.expanduser(self.scripts[thing])
         self.scripts=scripts
+        """for thing in files:
+            self.files[os.path.join(os.getcwd(), program, thing)]=os.path.expanduser(files[thing]) 
+        for thing in scripts:
+            self.scripts[os.path.join(os.getcwd(), program, thing)]=os.path.expanduser(scripts[thing])"""
 
     def print_files(self):
         for thing in self.files:
-            print(thing)
+            print(thing, "->", self.files[thing])
 
-    def write_files(self, backup=True):
-        
+    def write_files(self, backup=True):  
+        if backup:
+            if not os.path.exists(os.path.join(backup_folder, self.program)):
+                os.makedirs(os.path.join(backup_folder, self.program))
+        for thing in self.files:
+            if os.path.exists(self.files[thing]):
+                if backup:
+                    if os.path.exists(os.path.join(backup_folder, self.program, thing)):
+                        os.remove(os.path.join(backup_folder, self.program, thing))
+                    shutil.move(self.files[thing], os.path.join(backup_folder, self.program, thing))
+                else:
+                    os.remove(self.files[thing])
+            os.symlink(os.path.join(os.getcwd(), self.program, thing), self.files[thing])
 
-backup_folder="~/.mtyler_dot_backup"
 
 def install():
     with open('locations.json') as data_file:
@@ -24,8 +48,17 @@ def install():
     prog_categories=[]
     for prog_type in data:
         prog_categories.append(prog_files(prog_type, data[prog_type]["files_to_link"], data[prog_type]["scripts_to_run"]))
-    for i in prog_categories:
-        i.print_files()
+    if os.path.exists(backup_folder):
+        print("\n\nBackup directory detected at:", backup_folder, "\nIf you install, you'll overwrite any files here which may remove your original configs.")
+        check=None
+        while check!="y":
+            check=input("Do you want to continue installing?[y/n]: ")
+            if check=="n":
+                sys.exit()
+    print("Writing files...")
+    for prog in prog_categories:
+        prog.write_files(True)
+    print("Done")
 
 def uninstall():
     pass
@@ -43,4 +76,6 @@ def main():
 
 
 if __name__=='__main__':
+    #Change the current working directory, so all the files can be found
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
     main()
